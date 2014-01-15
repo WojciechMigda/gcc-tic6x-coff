9423e6152f45e156114b20e5fadad7edbb7a6775
Wojciech Migda <wojtek.golf@interia.pl>
2014-01-16 00:52:15 +0100

    first fixes, most of the new stuff goes into c6x/coff.h

--- gcc/config/c6x/coff.h
+++ gcc/config/c6x/coff.h
@@ -18,3 +18,47 @@
       sprint_ul (__p, (unsigned long) (NUM));			\
     }								\
   while (0)
+
+/* This says how to output assembler code to declare an
+   uninitialized external linkage data object.  Under SVR4,
+   the linker seems to want the alignment of data objects
+   to depend on their types.  We do exactly that here.  */
+
+#define COMMON_ASM_OP	"\tgcc/config/c6x/coff.h: COMMON_ASM_OP\n\t.comm\t"
+
+/* The following macro defines the format used to output the second
+   operand of the .type assembler directive.  Different svr4 assemblers
+   expect various different forms for this operand.  The one given here
+   is just a default.  You may need to override it in your machine-
+   specific tm.h file (depending upon the particulars of your assembler).  */
+
+#define TYPE_OPERAND_FMT	"@%s"
+
+/* Define the strings used for the special svr4 .type and .size directives.
+   These strings generally do not vary from one system running svr4 to
+   another, but if a given system (e.g. m88k running svr) needs to use
+   different pseudo-op names for these, they may be overridden in the
+   file which includes this one.  */
+
+#define TYPE_ASM_OP	"\tgcc/config/c6x/coff.h: TYPE_ASM_OP\n\t.type\t"
+#define SIZE_ASM_OP	"\tgcc/config/c6x/coff.h: SIZE_ASM_OP\n\t.size\t"
+
+
+/* Write the extra assembler code needed to declare a function's result.
+   Most svr4 assemblers don't require any special declaration of the
+   result value, but there are exceptions.  */
+
+#ifndef ASM_DECLARE_RESULT
+#define ASM_DECLARE_RESULT(FILE, RESULT)
+#endif
+
+/* This is how to allocate empty space in some section.  The .zero
+   pseudo-op is used for this on most svr4 assemblers.  */
+
+#define SKIP_ASM_OP	"\tgcc/config/c6x/coff.h: SKIP_ASM_OP\n\t.zero\t"
+
+#undef  ASM_OUTPUT_SKIP
+#define ASM_OUTPUT_SKIP(FILE, SIZE) \
+   fprintf ((FILE), "%s"HOST_WIDE_INT_PRINT_UNSIGNED"\n",\
+	    SKIP_ASM_OP, (SIZE))
+
